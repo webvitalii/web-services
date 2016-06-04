5a69a54868983e71002b05c010a8f7f2a0714a31
Vitalii <vitalii.skorokhod@gmail.com>
2016-06-04 00:06:54 -0500

    Added adsense code to sidebar

--- inc/_sidebar.php
+++ inc/_sidebar.php
@@ -1,6 +1,15 @@
 
 <aside class="fx-box-sm-3 fx-padding-all fx-bg-gray-lightest">
 
-	sidebar
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- web-profile-web-services-responsive -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5207574527239705"
+     data-ad-slot="8359592870"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
 </aside><!-- .fx-box -->
